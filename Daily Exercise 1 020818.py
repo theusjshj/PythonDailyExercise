4fcbd8b71ac7fd8eb2ab47a13836bd4b0b240105
Ryan Mottahed <118171@evit.com>
2018-02-12 12:08:22 -0700

    message

---
+++ Daily Exercise 1 020818.py
@@ -0,0 +1,11 @@
+# Ryan Mottahed – Python Daily Exercise – 020818
+
+# 1. Write a statement that concatenates at least two variables (you can have more than 2 for this practice).
+# use the string and integer functions, no arithmetic will be allowed for this exercise (i.e. - x*y), 
+# combine two variables for a string a variable. The key word here is CONCATENATE.
+
+first = "Hello"
+last = "World"
+worldnum = 2
+
+print(first + " " + last + " " + str(worldnum))
---
+++ Daily Exercise 2 020818.py
@@ -0,0 +1,10 @@
+# Ryan Mottahed – Python Daily Exercise – 020818
+
+# 2. Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old. Clue input()
+
+name = input("Enter name: ")
+age = input("Enter age: ")
+
+
+print(name + " turns 100 in " + )
